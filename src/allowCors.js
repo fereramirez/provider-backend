888cff0257da44824072fce34b5690025e59f583
Fer Ramirez <fer.eze.ram@gmail.com>
2022-09-23 21:00:40 -0300

    cors added

--- src/allowCors.js
+++ src/allowCors.js
@@ -1,9 +1,6 @@
 const allowCors = (fn) => async (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://providerstore.vercel.app/"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader(
