3da5b59aa9b2260eef0ac23c84f8159874658471
FabricioJRepetto <fabricio.j.repetto@gmail.com>
2022-09-25 00:35:27 -0300

    start: connect mongo first on boot

--- src/database/db.js
+++ src/database/db.js
@@ -3,17 +3,17 @@ const mongoose = require("mongoose");
 const { DB_URL, MONGODB_URI } = process.env;
 
 const clientDb = () => {
-  return mongoose.connect(
-    DB_URL,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-    (err) => {
-      if (err) console.log(err);
-      else console.log("mongdb is connected");
-    }
-  );
+    return mongoose.connect(
+        DB_URL,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        },
+        (err) => {
+            if (err) console.log(err);
+            else console.log("mongdb is connected");
+        }
+    );
 };
 
 module.exports = clientDb;
