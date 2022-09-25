c2cacb7300cbf9c0f795e461e8b62393649351e7
Fer Ramirez <fer.eze.ram@gmail.com>
2022-09-24 21:25:48 -0300

    send email logs added

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -70,8 +70,6 @@ const sendEmail = async (email, subject, templateUrl, variables) => {
     console.log("---------------------------------------------------");
     console.log(subject);
     console.log("---------------------------------------------------");
-    console.log(html);
-    console.log("---------------------------------------------------");
 
     const mailOptions = {
       from: `Provider <${EMAIL_EPROVIDER}>`,
