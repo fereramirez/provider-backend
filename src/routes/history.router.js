a7c38d5d42bae6ab4c27592907908caf8a292542
Fer Ramirez <fer.eze.ram@gmail.com>
2022-09-27 23:59:52 -0300

    commented

--- src/routes/history.router.js
+++ src/routes/history.router.js
@@ -1,18 +1,16 @@
 const { Router } = require("express");
 const router = Router();
 const {
-    getHistory,
-    getSuggestion,
-    postVisited,
-    postSearch,
+  getHistory,
+  getSuggestion,
+  postVisited,
+  postSearch,
 } = require("../controllers/history.ctrl.js");
-const {
-    verifyToken,
-} = require("../middlewares/verify");
+const { verifyToken } = require("../middlewares/verify");
 
-router.get("/", verifyToken, getHistory);
+/* router.get("/", verifyToken, getHistory);
 router.post("/suggestion", getSuggestion);
 router.post("/search/:search", verifyToken, postSearch);
-router.post("/visited", verifyToken, postVisited);
+router.post("/visited", verifyToken, postVisited); */
 
 module.exports = router;
