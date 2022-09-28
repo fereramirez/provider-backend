b468d7069b87063fa9d08eadf237a59dee8c4802
Fer Ramirez <fer.eze.ram@gmail.com>
2022-09-28 01:18:10 -0300

    log added

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 const app = require("./src/app.js");
 const PORT = process.env.PORT || 4000;
+console.log("PORT", PORT);
 const {
-    salesChecker,
-    flashSales,
-    salesMaker,
+  salesChecker,
+  flashSales,
+  salesMaker,
 } = require("./src/jobs/salesMaker");
 const { deliveryGuy, deliveryUpdater } = require("./src/jobs/deliveryGuy");
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}.`);
+  console.log(`Server listening on port ${PORT}.`);
 });
 
 //: ---------------- START JOB
 flashSales.start();
 console.log(
-    flashSales.running
-        ? "# Cron Job (flashSales): Running."
-        : "# Cron Job (flashSales): Not executed."
+  flashSales.running
+    ? "# Cron Job (flashSales): Running."
+    : "# Cron Job (flashSales): Not executed."
 );
 salesChecker();
 // salesMaker();
 
 deliveryUpdater.start();
 console.log(
-    deliveryUpdater.running
-        ? "# Cron Job (deliveryUpdater): Running."
-        : "# Cron Job (flashSales): Not executed."
+  deliveryUpdater.running
+    ? "# Cron Job (deliveryUpdater): Running."
+    : "# Cron Job (flashSales): Not executed."
 );
 deliveryGuy();
