05c605d6bd073cd99feb2955b63e31c6bffc2bdd
FabricioJRepetto <fabricio.j.repetto@gmail.com>
2022-09-28 18:49:22 -0300

    update

--- src/jobs/deliveryGuy.js
+++ src/jobs/deliveryGuy.js
@@ -1,4 +1,5 @@
 const CronJob = require('cron').CronJob;
+const Notifications = require('../models/Notifications');
 const order = require('../models/order');
 
 const deliveryGuy = async () => {
@@ -8,11 +9,28 @@ const deliveryGuy = async () => {
     for (const o of orders) {
         if ((Date.now()) - new Date(o.delivery_date) >= 0) {
             deliveries += 1;
-            await order.findByIdAndUpdate(o._id, {
-                '$set': {
-                    delivery_status: 'dispatched'
-                }
-            })
+
+            const newOrder = await order.findByIdAndUpdate(o._id,
+                {
+                    '$set': {
+                        delivery_status: 'dispatched'
+                    }
+                },
+                { new: true }
+            );
+
+            const notif = await Notifications.findOne({ user_id: newOrder.user });
+            if (notif) {
+                notif.notif_list.push({
+                    notif_type: 'success',
+                    title: '¡Tu pedido fue entregado!',
+                    description: `Tu orden del ${new Date(newOrder.created_at).toLocaleString("es-Ar", { timeZone: "America/Buenos_Aires" }).split(', ')[0]} ha sido entregada. ¡Esperamos que disfrutes de ${newOrder.products.length > 1 ? 'tus nuevos productos!' : 'tu nuevo producto!'} No olvides que ahora puedes crear una reseña ${newOrder.products.length > 1 ? 'de los productos.' : 'del producto.'}`,
+                    link: newOrder.products.length > 1 ? false : `/details/${newOrder.products[0].product_id}`,
+                    date: new Date().toLocaleString("es-Ar", { timeZone: "America/Buenos_Aires" }),
+                    seen: false
+                })
+                await notif.save();
+            }
         };
     };
     return `// ${deliveries || 'No'} packages delivered.`;
--- src/models/order.js
+++ src/models/order.js
@@ -61,12 +61,7 @@ orderSchema.virtual("description").get(function () {
 orderSchema.pre('save', async function (next) {
     // convertir la zonahoraria a -3 (-10800000) ?
     // 36hrs de expiración para MP (259200000) (stripe no acepta mas de 24hr)
-
-    // este campo se crea solo ???
     this.created_at = Date.now();
-
-    //! volver a ver: POST deploy del front, revisar si Date.now() funciona bien, sino usar new Date().getTime()
-    // ! debería funcionar pero... REVISAR REVISAR
 
     this.expiration_date_from = new Date(Date.now() - 10800000).toISOString().slice(0, -1) + '-03:00';
     this.expiration_date_to = new Date(Date.now() + 248400000).toISOString().slice(0, -1) + '-03:00';
